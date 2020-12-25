b95b57d8ade4e146407569bf6f3cc755d7b68dea
Savacano_28 <savacano28@users.noreply.github.com>
2020-12-25 12:20:40 +0100

    q4

--- exercises_mongodb/projet/queries_results.js
+++ exercises_mongodb/projet/queries_results.js
@@ -24,6 +24,7 @@ db.tournois.aggregate([{$lookup: {from: "pays", localField:"pays_id",foreignFiel
 
 //4.Nom des joueurs français qui jouent dans une équipe de double.
 db.equipes.aggregate([{ $project: {joueur:"$joueur0_id", _id: 0 } },{ $unionWith: { coll: "equipes", pipeline: [ { $project: { joueur:"$joueur1_id", _id: 0 } } ]} },{$group:{_id:"$joueur"}},{$lookup: {from: "joueurs", localField:"_id",foreignField:"_id",as: "joueur"}},{$match:{"joueur.pays_id":2}},{$addFields:{joueur_nom:"$joueur.nom"}},{$project:{"joueur_nom":1,_id:0}} ]);
+db.equipes.aggregate([{ $project: {joueur:"$joueur0_id", _id: 0 } },{ $unionWith: { coll: "equipes", pipeline: [ { $project: { joueur:"$joueur1_id", _id: 0 } } ]} },{$group:{_id:"$joueur"}},{$lookup: {from: "joueurs", localField:"_id",foreignField:"_id",as: "joueur"}},{$lookup:{from:"pays", localField:"joueur.pays_id",foreignField:"_id",as:"pays"}},{$match:{"pays.nom_pays":"France"}},{$addFields:{joueur_nom:"$joueur.nom"}},{$project:{"joueur_nom":1,_id:0}} ]);
 
 { "joueur_nom" : [ "Monfils" ] }
 { "joueur_nom" : [ "Gasquet" ] }
