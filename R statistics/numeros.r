2803307ab3f323d8461e392ee9160d1f6ec5fb42
Savacano_28 <savacano28@users.noreply.github.com>
2021-01-07 22:47:52 +0100

    mnist clustering

---
+++ R statistics/numeros.r
@@ -0,0 +1,20 @@
+sample1<-sample_n(data.frame(train), 10)
+sample_sans_label<-sample1[1,1:785]
+mnist_clustering=kmeans(sample_sans_label,centers=10,nstart = 10)
+mnist_centers <- mnist_clustering$centers
+
+# Plot typical cluster digits
+par(mfrow = c(2, 5), mar=c(0.5, 0.5, 0.5, 0.5))
+layout(matrix(seq_len(nrow(mnist_centers)), 2, 5, byrow = FALSE))
+for(i in seq_len(nrow(mnist_centers))) {
+  image(matrix(mnist_centers[i, ], 28, 28)[, 28:1], 
+        col = gray.colors(12, rev = TRUE), xaxt="n", yaxt="n")
+}
+
+
+inertie = c()
+for (k in 1:15){
+res=kmeans(sample1,centers=k,nstart = 10)
+inertie[k]=res$tot.withinss}
+plot(1:15,inertie,type='l')
+res_ka=kmeans(sample1,centers=3,nstart = 10)
