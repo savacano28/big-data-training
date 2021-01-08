b0a701b3c82caa2b9235c6a5ae091cd8734cedb1
Savacano_28 <savacano28@users.noreply.github.com>
2021-01-08 21:23:55 +0100

    Update knn.r

--- R statistics/knn.r
+++ R statistics/knn.r
@@ -6,3 +6,10 @@ tl=data[5001:6000,786]
 clas=knn(s,t,sl,10)
 mean(clas==tl)
 [1] 0.926
+
+tbc=NULL
+for(k in 1:20){
+clas=knn(s,t,sl,k)
+tbc[k]=mean(clas==tl)}
+
+plot(1:20,tbc,ylim=c(0,1),xlab='k',ylab='taux de bons classements')
