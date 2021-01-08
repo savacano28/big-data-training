794610c315caaaad71e17ee3ff11dd3855228d7c
Savacano_28 <savacano28@users.noreply.github.com>
2021-01-08 19:44:01 +0100

    Update numeros.r

--- R statistics/numeros.r
+++ R statistics/numeros.r
@@ -1,22 +1,27 @@
+#clustering
+install.packages("dplyr")
+library(dplyr)
 sample1<-sample_n(data.frame(train), 1000)
-sample_sans_label<-sample1[1,1:785]
+sample_sans_label<-sample1[,2:785]
 
-inertie = c()
-for (k in 1:15){
-res=kmeans(sample_sans_label,centers=k,nstart = 10)
-inertie[k]=res$tot.withinss}
-plot(1:15,inertie,type='l')
-
-mnist_clustering=kmeans(sample_sans_label,centers=10,nstart = 10)
+mnist_clustering=kmeans(sample_sans_label,centers=9,nstart = 20)
 mnist_centers <- mnist_clustering$centers
 
 # Plot typical cluster digits
-par(mfrow = c(2, 5), mar=c(0.5, 0.5, 0.5, 0.5))
-layout(matrix(seq_len(nrow(mnist_centers)), 2, 5, byrow = FALSE))
-for(i in seq_len(nrow(mnist_centers))) {
-  image(matrix(mnist_centers[i, ], 28, 28)[, 28:1], 
-        col = gray.colors(12, rev = TRUE), xaxt="n", yaxt="n")
-}
+par(mfrow=c(3,4))
+for (i in 1:10) show_digit(mnist_centers[i,])
+
+#inertia
+inertie = c()
+for (k in 1:9){
+res=kmeans(sample_sans_label,centers=k,nstart = 10)
+inertie[k]=res$tot.withinss}
+plot(1:9,inertie,type='l')
+
+#pca
+library(FactoMineR)
+sample.pca <- PCA(sample1, graph=FALSE)
+plot(sample.pca, choix= "ind",col.ind = pca$cluster,graph.type = "classic")
 
 
 
