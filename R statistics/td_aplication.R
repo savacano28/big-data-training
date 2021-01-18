d2bbfe3896dc75b6f02b5154a1a2b9157615d8e0
Savacano_28 <savacano28@users.noreply.github.com>
2021-01-18 19:45:16 +0100

    Update td_aplication.R

--- R statistics/td_aplication.R
+++ R statistics/td_aplication.R
@@ -13,4 +13,13 @@ res.pca
 res.pca$eig
 res.pca$var
 
+inertie=c()
+for(k in 2:10){
+res=kmeans(visaData[,-c(2,4,6,8,9,22,40,45)],centers=k,nstart = 10)
+inertie[k-1]=res$tot.withinss}
+plot(2:10,inertie,type='l')
+
+res=res=kmeans(visaData[,-c(2,4,6,8,9,22,40,45)],centers=4,nstart = 10)
+str(visaData)
+
 ##http://www.sthda.com/english/wiki/reading-data-from-txt-csv-files-r-base-functions
