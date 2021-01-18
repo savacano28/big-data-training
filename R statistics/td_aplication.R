626f49996054e9dd101d52ec1aef313ea2b75b81
Savacano_28 <savacano28@users.noreply.github.com>
2021-01-18 18:42:27 +0100

    Update td_aplication.R

--- R statistics/td_aplication.R
+++ R statistics/td_aplication.R
@@ -2,6 +2,15 @@ install.packages("readtext")
 visaData<-read.delim("/home/scasanova/Desktop/R statistics/VisaPremier.txt", header = TRUE, sep = "\t")
 summary(visaData)
 colnames(visaData)
-
+rownames(visaData)
+visaData[0,]
+res.pca<-PCA(visaData,quali.sup=c(2,4,6,8,9,22,40,45),graph=FALSE)
+plot(res.pca,choix = "var")
+plot(res.pca,choix = "ind")
+barplot(res.pca$eig[,2],main="Pourcentage de variance expliquée")
+barplot(res.pca$eig[,3],main="Pourcentage de variance expliquée")
+res.pca
+res.pca$eig
+res.pca$var
 
 ##http://www.sthda.com/english/wiki/reading-data-from-txt-csv-files-r-base-functions
